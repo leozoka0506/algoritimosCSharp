6cf09bd7fcb17e85810e448071dbafcc3ceddd08
leozoka0506 <126924418+leozoka0506@users.noreply.github.com>
2023-05-08 20:00:51 -0300

    add insert sort

---
+++ ex1/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] vetor = new int[10] { 3,1,5,7,3,9,8,2,0,4};
+int temp = 0;
+
+for  (int i = 0; i < vetor.Length - 1; i++)
+{
+    for (int j = 0; j < vetor.Length- 1; j++)
+    {
+        if (vetor[j] > vetor[j + 1])
+        {
+            temp = vetor[j];
+            vetor[j] = vetor[j + 1];
+            vetor[j + 1] = temp;
+        }
+
+    }
+}
+    
+foreach (int i in vetor)
+{
+    Console.WriteLine(i);
+}
